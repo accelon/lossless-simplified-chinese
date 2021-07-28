d38e78c90ed3bd67b897cada8129856c2ff00fdd
unknown <yap>
2021-07-28 09:23:34 +0630

    stricter rule, skip Big5 char

--- test.js
+++ test.js
@@ -1,18 +1,38 @@
-import {toLosslessSim,fromLosslessSim} from './index.js'
-import {strictEqual} from 'assert'
+import {toSim,fromSim} from './index.js'
 let passcount=0;
-const testcount=3;
+const testcount=18;
 
 const text='大道之行也．天下為公．選賢與能．講信脩睦．故人不獨親其親．不獨子其子．使老有所終．壯有所用．幼有所長．矜寡孤獨廢疾者．皆有所養．男有分．女有歸．貨惡其棄於地也．不必藏於已．力惡其不出於身也．不必為已．是故謀閉而不興．盜竊亂賊而不作．故外戶而不閉．是謂大同';
-//console.log('orignal',text,'\nLossless Simplified',toLosslessSim(text),'\nrestore',fromLosslessSim(toLosslessSim(text)));
 
 const text2='廣式炒麵 複雜 覆滅';
 const text3='天后宮 後宮佳麗'
-console.log(text2,'>>',toLosslessSim(text2));
-passcount+= (fromLosslessSim(toLosslessSim(text))==text)?1:0;
-passcount+= (fromLosslessSim(toLosslessSim(text2))==text2)?1:0;
-passcount+= (fromLosslessSim(toLosslessSim(text3))==text3)?1:0;
+// console.log(text3,'>>',toSim(text3),fromSim(toSim(text3)));
+// console.log(text3,'>>',toSim(text3));
 
-console.log(text3,'>>',toLosslessSim(text3));
+
+passcount+=(toSim('樹幹')=='树幹')?1:0            //safe
+passcount+=(toSim('樹幹',true)=='树干')?1:0       //unsafe
+passcount+=(toSim('心願',true)=='心愿')?1:0       //safe
+passcount+=(toSim('餅乾')=='饼乾')?1:0            //safe
+passcount+=(toSim('餅乾',true)=='饼干')?1:0       //unsafe
+ 
+passcount+=(fromSim('饼干')  =='餅干')?1:0          //safe
+passcount+=(fromSim('饼干',1)=='餅乾')?1:0        //pick the best match
+passcount+=(fromSim('饼干',2)=='餅[乾幹干]')?1:0  //expand other possibility
+
+
+passcount+= (fromSim(toSim(text))==text)?1:0;
+passcount+= (fromSim(toSim(text2))==text2)?1:0;
+passcount+= (fromSim(toSim(text3))==text3)?1:0;
+
+passcount+=(toSim('無損簡體')=='无损简體' )       
+passcount+=(toSim('無損簡體',true)=='无损简体')   //強制轉換
+passcount+=(fromSim('无损简體')=='無損簡體'    )  
+passcount+=(fromSim('无损简体')=='無損簡体'     ) 
+passcount+=(fromSim('无损简体',1)=='無損簡體'    )
+passcount+=(fromSim('无损简体',2)=='無損簡[體体]')
+
+passcount+=(fromSim('树干',1)=='樹乾')
+
 
 console.log('Passed test',passcount,'test count',testcount);
